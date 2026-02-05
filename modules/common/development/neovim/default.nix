c2a092f36959004f8866c0d68077ae58dd19e719
Cephalode <git@cephalode.com>
2026-02-05 08:28:22 -0500

    feat: ✨ NixCats module to flake migration

--- modules/common/development/neovim/default.nix
+++ modules/common/development/neovim/default.nix
@@ -1,156 +1,140 @@
 {
-  inputs,
-  ...
-}:
-let
-  utils = inputs.nixCats.utils;
-in
-{
-  imports = [
-    inputs.nixCats.nixosModules.default
-  ];
-  config = {
-    nixCats = {
-      enable = true;
-      nixpkgs_version = inputs.nixpkgs;
-      addOverlays = [
-        (utils.standardPluginOverlay inputs)
-      ];
-      packageNames = [ "nixCats" ];
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixCats.url = "github:BirdeeHub/nixCats-nvim";
+  };
 
-      luaPath = ./.;
+  outputs = { self, nixpkgs, ... }@inputs: let
+    inherit (inputs.nixCats) utils;
+    luaPath = ./.;
+    forEachSystem = utils.eachSystem nixpkgs.lib.platforms.all;
+    extra_pkg_config = {
+      allowUnfree = true;
+    };
 
-      categoryDefinitions.replace = (
-        {
-          pkgs,
-          settings,
-          categories,
-          extra,
-          name,
-          mkPlugin,
-          ...
-        }@packageDef:
-        {
-          lspsAndRuntimeDeps = with pkgs; {
-            general = [
-              ripgrep
-              fd
-              fzf
-              tree-sitter
-              universal-ctags
-            ];
+    dependencyOverlays = [
+      (utils.standardPluginOverlay inputs)
+    ];
+    categoryDefinitions = { pkgs, settings, categories, extra, name, mkPlugin, ... }@packageDef: {
+      lspsAndRuntimeDeps = with pkgs; {
+        general = [
+          ripgrep
+          fd
+          fzf
+          tree-sitter
+          universal-ctags
+        ];
 
-            lint = [];
-            debug = [];
+        lint = [];
+        debug = [];
 
-            format = [
-              stylua
-              nodePackages.prettier
-              nixfmt-rfc-style
-            ];
+        format = [
+          stylua
+          nodePackages.prettier
+          nixfmt-rfc-style
+        ];
 
-            neonixdev = [
-              lua-language-server
-              marksman
-              nixd
-              pyright
-              typescript-language-server
-              zls
-            ];
-          };
+        neonixdev = [
+          lua-language-server
+          marksman
+          nixd
+          pyright
+          typescript-language-server
+          zls
+        ];
+      };
 
-          startupPlugins = with pkgs.vimPlugins; {
-            debug = [];
+      startupPlugins = with pkgs.vimPlugins; {
+        debug = [];
 
-            general = {
-              always = [
-                lze
-                lzextras
-                vim-repeat
-                plenary-nvim
-                (nvim-notify.overrideAttrs { doCheck = false })
-              ];
+        general = {
+          always = [
+            lze
+            lzextras
+            vim-repeat
+            plenary-nvim
+            (nvim-notify.overrideAttrs { doCheck = false; })
+          ];
 
-              extra = [
-                oil-nvim
-                nvim-web-devicons
-              ];
-            };
-          };
+          extra = [
+            oil-nvim
+            nvim-web-devicons
+          ];
+        };
+      };
 
-          optionalPlugins = with pkgs.vimPlugins; {
-            debug = {
-              default = [
-                nvim-dap
-                nvim-dap-ui
-                nvim-dap-virtual-text
-              ];
-            };
+      optionalPlugins = with pkgs.vimPlugins; {
+        debug = {
+          default = [
+            nvim-dap
+            nvim-dap-ui
+            nvim-dap-virtual-text
+          ];
+        };
 
-            lint = [
-              nvim-lint
-            ];
-            format = [
-              conform-nvim
-            ];
+        lint = [
+          nvim-lint
+        ];
+        format = [
+          conform-nvim
+        ];
 
-            markdown = [
-              markdown-preview-nvim
-              obsidian-nvim
-              # vim-pandoc
-            ];
+        markdown = [
+          markdown-preview-nvim
+          obsidian-nvim
+          # vim-pandoc
+        ];
 
-            neoixdev = [
-              lazydev-nvim
-            ];
+        neoixdev = [
+          lazydev-nvim
+        ];
 
-            general = {
-              blink = [
-                blink-cmp
-                blink-compat
-                cmp-cmd-line
-                colorful-menu-nvim
-                luasnip
-              ];
+        general = {
+          blink = [
+            blink-cmp
+            blink-compat
+            cmp-cmd-line
+            colorful-menu-nvim
+            luasnip
+          ];
 
-              treesitter = [
-              nvim-treesitter-textobjects
-              nvim-treesitter.withAllGrammars
+          treesitter = [
+            nvim-treesitter-textobjects
+            nvim-treesitter.withAllGrammars
+          ];
 
-              mini = [
-                mini-pick
-              ];
+          mini = [
+            mini-pick
+          ];
 
-              always = [
-                gitsigns-nvim
-                lualine-nvim
-                nvim-lspconfig
-                nvim-surround
-                vim-sleuth
-                vim-fugitive
-                vim-rhubarb
-              ];
+          always = [
+            gitsigns-nvim
+            lualine-nvim
+            nvim-lspconfig
+            nvim-surround
+            vim-sleuth
+            vim-fugitive
+            vim-rhubarb
+          ];
 
-              extra = [
-                comment-nvim
-                fidget-nvim
-                hlargs-nvim
-                image-nvim
-                indent-blankline-nvim
-                neotree
-                nui-nvim
-                pomo-nvim
-                smear-cursor-nvim
-                vim-be-good
-                vim-repeat
-                vim-startuptime
-                undotree
-                which-key-nvim
-              ];
-            };
-          };
-        }
-      );
+          extra = [
+            comment-nvim
+            fidget-nvim
+            hlargs-nvim
+            image-nvim
+            indent-blankline-nvim
+            neotree
+            nui-nvim
+            pomo-nvim
+            smear-cursor-nvim
+            vim-be-good
+            vim-repeat
+            vim-startuptime
+            undotree
+            which-key-nvim
+          ];
+        };
+      };
 
       python3.libaries = {
         test = (_:[]);
@@ -207,7 +191,7 @@ in
             neonixdev = true;
             lint = true;
             format = true;
-            lspDebugMode = false
+            lspDebugMode = false;
           };
           extra = {
             nixdExtras = {
@@ -217,7 +201,43 @@ in
         };
       };
 
-      defaultPackageName = "nixCats";
-    };
+      defaultPackageName = "nvim";
+
+    in
+    forEachSystem (system: let
+      nixCatsBuilder = utils.baseBuilder defaultPackageName;
+
+      pkgs = import nixpkgs { inherit system; };
+
+    in {
+      packages = utils.mkAllWithDefault defaultPackage;
+
+      devShells = {
+        default = pkgs.mkShell {
+          name = defaultPackageName;
+          packages = [ defaultPackage ];
+          inputsFrom = [ ];
+          shellHook = ''
+          '';
+        };
+      };
+    }) // (let
+      nixosModule = utils.mkNixosModules {
+        moduleNamespace = [ defaultPackageName ];
+        inherit defaultPackageName dependencyOverlays luaPath
+          categoryDefinitions packageDefinitions extra_pkg_config nixpkgs;
+      };
+      nixosModule = utils.mkNixosModules {
+        moduleNamespace = [ defaultPackageName ];
+        inherit defaultPackageName dependencyOverlays luaPath
+          categoryDefinitions packageDefinitions extra_pkg_config nixpkgs;
+      };
+    in {
+      overlay = utils.makeOverlays luaPath {
+        inherit nixpkgs dependencyOverlays extra_pkg_config;
+      } categoryDefinitions packageDefinitions defaultPackageName;
+
+      nixosModules.default = nixosModule;
+
   };
 }
