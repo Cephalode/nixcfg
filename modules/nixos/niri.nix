079db61587a74eb823ddb889cb9f5929cbdea367
Cephalode <git@cephalode.com>
2026-08-05 16:54:47 -0400

    fix: greetd config parse error on loligo
    Multi-line Nix string with backslash continuations was producing
    malformed TOML in greetd's config.toml. Flattened to single line.

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -56,12 +56,7 @@
       enable = true;
       settings = {
         default_session = {
-          command = ''
-            ${pkgs.tuigreet}/bin/tuigreet 
-            --time --remember \
-            --theme 'container=black;border=cyan;title=white;text=white;prompt=white;input=white;action=cyan;button=cyan'\
-            --cmd ${pkgs.niri}/bin/niri
-          '';
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --theme 'container=black;border=cyan;title=white;text=white;prompt=white;input=white;action=cyan;button=cyan' --cmd ${pkgs.niri}/bin/niri";
           user = "greeter";
         };
       };
