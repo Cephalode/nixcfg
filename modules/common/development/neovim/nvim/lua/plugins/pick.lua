8d8e93b93ae68cd47773e758e83f2100063f8e82
Cephalode <invoice@cephalode.com>
2026-08-21 16:27:51 -0400

    nvim: enable mini.animate cursor smear

--- modules/common/development/neovim/nvim/lua/plugins/pick.lua
+++ modules/common/development/neovim/nvim/lua/plugins/pick.lua
@@ -6,6 +6,7 @@ return {
     after = function()
       require("mini.pick").setup()
       require("mini.statusline").setup()
+      require("mini.animate").setup({ scroll = { enable = false }, resize = { enable = false } })
       vim.keymap.set("n", "<leader>ff", function() require("mini.pick").builtin.files() end, { desc = "Find files" })
       vim.keymap.set("n", "<leader>fg", function() require("mini.pick").builtin.grep_live() end, { desc = "Live grep" })
       vim.keymap.set("n", "<leader>fb", function() require("mini.pick").builtin.buffers() end, { desc = "Buffers" })
